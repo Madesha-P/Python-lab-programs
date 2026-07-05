14a2f475cca49515aa6a17764d3766870d12c116
Madesha-P <madesha.work@gmail.com>
2026-07-05 22:02:23 +0530

    modification: in detailed way

--- exe7.py
+++ exe7.py
@@ -1,21 +1,26 @@
-w=[]
-n=int(input("Enter the number of words:"))
+# where the keys are the lengths of the words and the values are lists of words that have that length.
+# * The list inside the dictionary is used to store multiple words of the same length, 
+# * allowing for efficient grouping and retrieval of words based on their lengths.
 
-for i in range (n):
-    word=input("Enter the word:")
-    w.append(word)
-
+word_list=[]
 word_dict={}
 
-for i in w:
-    length=len(i)
+N=int(input("Enter the number of words :"))
+
+for i in range (N):
+    word=input(f"Enter the word {i+1} :")
+    word_list.append(word)
+
+for w in word_list:
+    length=len(w)
     if length in word_dict:
-        word_dict[length].append(i)
+        word_dict[length].append(w)
+        # Here we are appending the word to the list inside the dictionary corresponding to its length
 
     else:
-        word_dict[length]=[i]
-        
+         word_dict[length]=[w] 
+        # this is a string instead of a list, so we need to put it in a list
 
-print("Words grouped by length:-")
+print("the words grouped with length are :")
 
 print(word_dict)
